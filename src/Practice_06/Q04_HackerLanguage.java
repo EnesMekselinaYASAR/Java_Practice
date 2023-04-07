7f1978dd1bf5469df1556acdd024b4e653e1107c
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-07 23:05:39 +0300

    07.04.2023

--- src/Practice_06/Q04_HackerLanguage.java
+++ src/Practice_06/Q04_HackerLanguage.java
@@ -14,4 +14,25 @@ public class Q04_HackerLanguage {
     //    hackerDili("java ile hersey guzel")
     //    j4v4 1l3 h3r53y guz3l
     //    İpucu harfleri değiştirin ve ekrana yazdırın.
+
+    public static void main(String[] args) {
+
+        int arr[][] = {{7, 12, 97, 23}, {543, 23, 675}, {2}, {2, 33, 6, 3, 7}};
+
+        System.out.println("elemanSayisi(arr) = " + elemanSayisi(arr));
+
+    }
+
+    public static int elemanSayisi(int[][] arr) {
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) { // output for
+            for (int j = 0; j < arr[i].length; j++) { // inner for
+                count++;
+
+            }
+        }
+        return count;
+    }
 }
